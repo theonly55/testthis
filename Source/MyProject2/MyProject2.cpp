6ff909e1857ea939d6973da61fe85444f158a57b
Paul Pews <socer_kid@hotmail.com>
2016-01-20 19:49:59 -0500

    a

---
+++ Source/MyProject2/MyProject2.cpp
@@ -0,0 +1,7 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "MyProject2.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, MyProject2, "MyProject2" );
+
+//hello world!!!!!!
---
+++ Source/MyProject2/MyProject2GameMode.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameMode.h"
+#include "MyProject2GameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MYPROJECT2_API AMyProject2GameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
